7440dffac16ba4d202a8c1ea3f443a17c9dd68ea
RafayelGardishyan <rgardishyan@gmail.com>
2017-09-24 16:29:23 +0200

    Edit

--- django_books/settings.py
+++ django_books/settings.py
@@ -155,13 +155,4 @@ MEDIA_ROOT  = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/media/'
 
-CKEDITOR_JQUERY_URL = 'https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js'
 
-CKEDITOR_UPLOAD_PATH = 'uploads/'
-CKEDITOR_IMAGE_BACKEND = "pillow"
-
-CKEDITOR_CONFIGS = {
-    'default': {
-        'toolbar': None,
-    },
-}
