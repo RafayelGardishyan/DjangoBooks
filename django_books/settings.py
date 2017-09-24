6dba1a4fd64dbfa40fc3ab2d84c917ee5a9d90e1
RafayelGardishyan <rgardishyan@gmail.com>
2017-09-24 16:46:37 +0200

    Heroku

--- django_books/settings.py
+++ django_books/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -155,4 +156,10 @@ MEDIA_ROOT  = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/media/'
 
+# Update database configuration with $DATABASE_URL.
+
+# Update database configuration with $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
