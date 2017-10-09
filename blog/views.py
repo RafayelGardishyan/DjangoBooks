5f31a9278bea111f4ec920c45e16cf8ecb14c0fa
RafayelGardishyan <rgardishyan@gmail.com>
2017-10-09 15:32:38 +0200

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -53,11 +53,11 @@ def api_authors_single(request, author_name):
     info = author.author_info
 #     booksraw = Post.objects.get(author=author)
 #     books = Post.objects.values('title')
-    from django.core import serializers
-    books = serializers.serialize('json', Post.objects.get(author=author), fields=('title',))
+#     from django.core import serializers
+#     books = serializers.serialize('json', Post.objects.get(author=author), fields=('title',))
     
     
-    authorlist[name] = {'id': author.id, 'name': name, 'info': info, 'books': books }
+    authorlist[name] = {'id': author.id, 'name': name, 'info': info}
 
     return JsonResponse(authorlist)
 
